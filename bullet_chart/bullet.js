b238ea7884e446dc7f0ecb791e8fdb30134131cd
Zheng, Yuhang <YZheng83@MassMutual.com>
2017-12-20 16:43:44 -0500

    prepare

---
+++ bullet_chart/bullet.js
@@ -0,0 +1,138 @@
+(function(){
+    d3.bullet = function(){
+        var orient = "left",
+            reverse = false,
+            duration = 0,
+            ranges = bulletRanges,
+            markers = bulletMarkers,
+            measures = bulletMeasures,
+            width = 380,
+            height = 30,
+            tickFormat = null;
+
+        //for each small multiple
+        function bullet(g){
+            g.each(function(d, i){
+                var rangez = ranges.call(this, d, i).slice().sort(d3.descending),
+                    markerz = markers.call(this, d, i).slice().sort(d3.descending),
+                    measurez = measures.call(this, d, i).slice().sort(d3.descending),
+                    g = d3.select(this);
+
+                //compute the new x-scale.
+                var x1 = d3.scaleLinear()
+                    .domain([0, Math.max(rangez[0], markerz[0], measurez[0])])
+                    .range(reverse ? [width, 0]:[0, width]);
+
+                //retrieve the old x-scale, if this is an update
+                var x0 = this.__chart || d3.scaleLinear()
+                    .domain([0, Infinity])
+                    .range(x1.range());
+
+                //stash the new scale
+                this.__chart__ = x1;
+
+                //derive width-scale from the x-scales.
+                var w0 = bulletWidth(x0),
+                    w1 = bulletWidth(x1);
+
+                //update the range rects
+                var range = g.selectAll("rect.range")
+                    .data(rangez);
+
+                range.enter().append("rect")
+                    .attr("class", function(d,i){ return "range s" + i;})
+                    .attr("width", w0)
+                    .attr("height", height)
+                    .attr("x", reverse? x0: 0)
+                    .transition()
+                    .duration(duration)
+                    .attr("width", w1)
+                    .attr("x", reverse? x1: 0);
+
+                range.transition()
+                    .duration(duration)
+                    .attr("x", reverse ? x1: 0)
+                    .attr("width", w1)
+                    .attr("height", height);
+
+                //update the measure rects.
+                var measure = g.selectAll("rect.measure")
+                    .data(measurez);
+
+                measure.enter().append("rect")
+                    .attr("class", function(d,i){return "measure s" + i;})
+                    .attr("width", w0)
+                    .attr("height", height/3)
+                    .attr("x", reverse?x0: 0)
+                    .attr("y", height/3)
+                    .transition()
+                    .duration(duration)
+                    .attr("width", w1)
+                    .attr("x", reverse?x1: 0);
+
+                measure.transition()
+                    .duration(duration)
+                    .attr("width", w1)
+                    .attr("height", height/3)
+                    .attr("x", reverse ? x1: 0)
+                    .attr("y", height/3);
+                //question: why two transition ofr measure and marze? delete one see what happen
+
+                //update the marker lines.
+                var marker = g.selectAll("line.marker")
+                    .data(markerz);
+
+                marker.enter().append("line")
+                    .attr("class", "marker")
+                    .attr("x1", x0)
+                    .attr("x2", x0)
+                    .attr("y1", height/6)
+                    .attr("y2", height*5/6)
+                    .transition()
+                    .duration(duration)
+                    .attr("x1", x1)
+                    .attr("x2", x1);
+
+                marker.transition()
+                    .duration(duration)
+                    .attr("x1", x1)
+                    .attr("x2", x1)
+                    .attr("y1", height/6)
+                    .attr("y2", height*5/6);
+
+                //compute the tick format
+                var format = tickFormat || x1.tickFormat(8);
+
+                //update the tick groups
+                var tick = g.selectAll('g.tick')
+                    .data(x1.ticks(8), function(d){
+                        return this.textContent || format(d);
+                    });
+
+                //initialize the ticks with the old scale, x1
+
+                var tickEnter = tick.enter().append("g")
+                    .attr("class", "tick")
+                    .attr("transform", bulletTranslate(x0))
+                    .style("opacity", 1e-6);
+
+                tickEnter.append("line")
+                    .attr("y1", height)
+                    .attr("y2", height*7/6);
+
+                tickEnter.append("text")
+                    .attr("text-anchor", "middle")
+                    .attr("dy", "1em")
+                    .attr("y", height*7/6)
+                    .text(format);
+
+
+                
+
+            })
+        }
+
+
+
+    }
+}
